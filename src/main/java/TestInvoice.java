9a68fbb0af71671ea877d947d87a5e429c760b55
mahendragondi <mahendra.gondi@gmail.com>
2025-05-25 10:10:34 +0100

    generated files

--- src/main/java/TestInvoice.java
+++ src/main/java/TestInvoice.java
@@ -17,7 +17,7 @@ public class TestInvoice {
         JAXBContext context = JAXBContext.newInstance(ObjectFactory.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
         StreamSource ss = new StreamSource();
-        ss.setReader(new StringReader(Files.readString(Paths.get("/Users/mahendragondi/Downloads/UBL-Invoice-2.1-Example-Trivial.xml"))));
+        ss.setReader(new StringReader(Files.readString(Paths.get("/Users/mahendragondi/Downloads/UBL-Invoice-2.1.xml"))));
         JAXBElement<InvoiceType> invoice =  unmarshaller.unmarshal(ss, InvoiceType.class);
         invoice.getValue().getID().getValue();
     }
